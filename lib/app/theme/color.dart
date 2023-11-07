e90e53d08e4caeeb0ea0c0aa58787fcbb91446be
kasahun-yb <abushmeta@gmail.com>
2023-11-07 14:31:58 +0300

    Initial commit

---
+++ lib/app/theme/color.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class color{
+   Color bottonColor=HexColor("#3797EF");
+   Color textButtonColor=HexColor("#3797EF");
+   Color textButtonColor2=HexColor("#262626");
+   Color textButtonColor3=HexColor("#66000000");
+   Color searchContainer = HexColor("#31767680");
+   Color backgroundColor=HexColor("#FAFAFA");
+   Color tabBorder = HexColor("#2E3C3C43");
+   
+   Color storyBorder=HexColor("#C7C7CC");
+
+   Color storyBorderYellow=HexColor("#FBAA47");
+   Color storyBorderRed=HexColor("#D91A46");
+   Color storyBorderPurple=HexColor("#A60F93");
+
+   Color liveStoryBorderPink=HexColor("#C60188");
+   Color liveStoryBorderRed=HexColor("#E20337");
+   Color liveStoryBorderPurple=HexColor("#7700C3");
+
+   Color liveboxRed=HexColor("#E10038");
+   Color liveBoxPink=HexColor("#C90083");
+   Color liveBoxMiddelRed=HexColor("#C90083");
+
+
+   
+
+
+
+
+}
